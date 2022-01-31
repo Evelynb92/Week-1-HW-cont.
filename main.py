0d58cc9e588d9068f7765040a106918b216f840d
EvelynBrooks1 <98491379+Evelynb92@users.noreply.github.com>
2022-01-31 19:07:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+print('12 Questions ?','\n')
+print('Name')
+print('Age')
+print('Birthdate')
+print('Zodiac Sign')
+print('Height')
+print('Favorite Hobby')
+print('Favorite Vacation Spot')
+print('Favorite Food')
+print('Introvert or Extrovert')
+print('Last TV Binge')
+print('Favorite Holiday')
+print('Go-to Guilty Pleasure','\n')
+
+print('12 Anwsers','\n')
+
+a= "Evelyn"
+b= "29"
+c= "08/09/1992"
+d= "Leo"
+e= "5 feet 3 inches"
+f= "Traveling"
+g= "Colombia"
+h= "French Fries"
+i= "Intovert"
+j= "Mindy's Project"
+k= "Halloween"
+l= "Whining and complaining"
+
+print('Name:', a)
+print('Age:', b)
+print('Birthdate:', c)
+print('Zodiac Sign:', d)
+print('Height:', e)
+print('Favorite Hobby:', f)
+print('Favorite Vacation Spot:', g)
+print('Favorite Food;', h)
+print('Introvert or Extrovert:', i)
+print('Last TV Binge:', j)
+print('Favorite Holiday:', k)
+print('Go-to Guilty Pleasure:', l,'\n')
+
+
+print('Her name is', a, 'she is', b, 'years old. Her birthdate is', c, 'making her zodiac sign a', d,'.She is standing tall at', e, '. Her favorite hobby is', f, 'and her favorite vacation spot is', g, 'Some more fun facts are her favorite food is', h, ', she considers herself to be an', i, 'the last show she has binged watched is', j, 'Her favorite holiday is',k, 'and lastly her go-to guilty pleasure is', l,'.')
